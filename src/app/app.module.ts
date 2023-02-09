fe69abb0820fdf03170f29770164279e15d4691f
yosielvelasco <a2183410361@alumnos.uat.edu.mx>
2023-02-09 12:53:55 -0600

    actualizacion
    actualizacion

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContentComponent } from './content/content.component';
+import { InicioComponent } from './content/inicio/inicio.component';
+import { FooterComponent } from './footer/footer.component';
+import { RegistroComponent } from './registro/registro.component';
+import { SesionComponent } from './sesion/sesion.component';
+import { PracticasComponent } from './practicas/practicas.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    ContentComponent,
+    InicioComponent,
+    FooterComponent,
+    RegistroComponent,
+    SesionComponent,
+    PracticasComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
